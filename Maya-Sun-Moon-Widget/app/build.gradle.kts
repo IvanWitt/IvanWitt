0a9af36110576f1bcc5e0f28127c3d2352e7927e
IvanWitt <svc.witt@gmail.com>
2026-07-26 23:26:51 +0300

    Bump cleaned classic widget build to v0.3.3

--- Maya-Sun-Moon-Widget/app/build.gradle.kts
+++ Maya-Sun-Moon-Widget/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ivanwitt.mayasunmoon"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16
-        versionName = "0.3.2"
+        versionCode = 17
+        versionName = "0.3.3"
     }
 
     compileOptions {
