e3fee50d2e263b7bb6a18cd4f290e27677345d6a
IvanWitt <svc.witt@gmail.com>
2026-07-27 20:43:17 +0300

    Bump Maya widget to v0.3.11

--- Maya-Sun-Moon-Widget/app/build.gradle.kts
+++ Maya-Sun-Moon-Widget/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ivanwitt.mayasunmoon"
         minSdk = 26
         targetSdk = 35
-        versionCode = 24
-        versionName = "0.3.10"
+        versionCode = 25
+        versionName = "0.3.11"
     }
 
     compileOptions {
